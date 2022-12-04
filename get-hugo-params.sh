d593d4ebaabfc37c0dd28e90c01d999fb9b89187
Michał Piotrowski <michal@mpprojekt.org>
2022-12-04 21:02:31 +0100

    Add fetch variables from env

--- get-hugo-params.sh
+++ get-hugo-params.sh
@@ -1,3 +1,4 @@
+# Fetches variables from a file and passes them to the container
 set -eu
 
 cd "$(dirname "${0}")"
@@ -8,5 +9,9 @@ if [ -f hugo_params ];then
       HUGO_PARAMS+="-e $param "
    done <hugo_params
 fi
+
+for param in $(env | grep HUGO_PARAMS); do
+      HUGO_PARAMS+="-e $param "
+done
 
 echo -e "${HUGO_PARAMS}"
