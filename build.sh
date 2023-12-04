f18fe6ad61f66b04a22bfe784493345a227a608c
Michał Piotrowski <michal@mpprojekt.org>
2023-12-04 02:04:40 +0100

    Fix

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ docker run --rm \
    -v $(pwd):/src $HUGO_PARAMS \
    --network host \
    -p 1313:1313 \
-   -u $(id -u):$(id -g) \
    --entrypoint "/bin/bash" \
    "${HUGO_IMAGE}" \
    -c "yarn install && ./node_modules/.bin/tailwindcss -i ./themes/tella/assets/css/main.css -o ./themes/tella/assets/css/style.css &&  hugo --verbose \
